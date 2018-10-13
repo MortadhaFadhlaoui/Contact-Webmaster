6b74f9aff65629676d552fe7b05676e3d8fd8b28
Fadhlaoui <mortadha.fadhlaoui@esprit.tn>
2018-10-13 21:31:06 +0100

    commit nodemailer

--- contact-webmaster-nodejs-app/routes/contact.route.js
+++ contact-webmaster-nodejs-app/routes/contact.route.js
@@ -1,26 +1,72 @@
 // routes/contact.route.js
 
-const express = require('express');
+const express = require("express");
 const app = express();
 const contactRoutes = express.Router();
+const nodemailer = require("nodemailer");
 
-// Require Contact model in our routes module
-let Contact = require('../models/Contact');
-
-// Defined store route
-contactRoutes.route('/add').post(function (req, res) {/* POST contact/add */
-    // create a new contact from Request client (json format)
-    let contact = new Contact(req.body);
-
-    //save contact object to database
-    contact.save()
-        .then(contact => {
-            res.status(200).json({'result': 'Contact in added successfully'});
-        })
-        .catch(err => {
-            res.status(400).json({'result': 'Unable to save to database'});
-        });
+// Configure mail transporter
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "mortadha.fadhlaoui@esprit.tn",
+    pass: "mewkhhitdxluqjah"
+  }
 });
 
+// Config mail sender
+const sender = "contact-webmaster@test.fr";
+
+// Require Contact model in our routes module
+let Contact = require("../models/Contact");
+
+// Defined store route
+contactRoutes.route("/add").post(function(req, res) {
+  /* POST contact/add */
+  // create a new contact from Request client (json format)
+  let contact = new Contact(req.body);
+
+  //save contact object to database
+  contact
+    .save()
+    .then(contact => {
+      sendMail(req.body);
+      res.status(200).json({ result: "Contact is added successfully" });
+    })
+    .catch(err => {
+      res.status(400).json({ result: "Unable to save to database" });
+    });
+});
+
+// Send mail function
+function sendMail(receiver) {
+  const mailOptions = {
+    from: "Webmaster <" + sender + ">",
+    to: receiver.email,
+    subject: "Confirmation of your sending",
+    text:
+      "Your recently send a message, We will try to answer you as much as possible.\n" +
+      "Message content :\n" +
+      "Name : " +
+      receiver.name +
+      "\n" +
+      "Email : " +
+      receiver.email +
+      "\n" +
+      "Phone number : " +
+      receiver.phone +
+      "\n" +
+      "Message : " +
+      receiver.message +
+      "."
+  };
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+}
 
 module.exports = contactRoutes;
